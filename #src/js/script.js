fd4dd757d3f5c0e1a974950350db1757d6c82b05
codeurient <codeurient@gmail.com>
2021-12-14 08:51:08 +0200

    create

---
+++ #src/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mainBg = document.querySelector(".page");
+  document.addEventListener("scroll", (evt) => {
+    let scroll =
+      evt.pageYOffset ||
+      (document.documentElement && document.documentElement.scrollTop) ||
+      (document.body && document.body.scrollTop);
+    mainBg.style.backgroundPosition = `center ${scroll / 2}px`;
+  });
+});
