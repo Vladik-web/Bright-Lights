c0dba9e522e846aee93eb3f3690c327d1962b5f7
codeurient <codeurient@gmail.com>
2021-12-21 17:11:12 +0200

    create mobile version

--- #src/js/headerFixed.js
+++ #src/js/headerFixed.js
@@ -13,7 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
       header.classList.remove("fixed");
     }
     line.style.width =
-      (scroll * 100) / document.body.offsetHeight + 17.5304 + "%";
+      (scroll * 100) / document.body.clientHeight + 17.5304 + "%";
   });
   82.4696;
 });
+
+/* open navigation */
+const headerMobile = document.querySelector(".header__mobile");
+const navClose = document.querySelector(".nav__close");
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".nav__list");
+function toggleActive() {
+  nav.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+headerMobile.addEventListener("click", toggleActive);
+navClose.addEventListener("click", toggleActive);
+if (document.body.clientWidth < 1023) {
+  navList.addEventListener("click", (evt) => {
+    const target = evt.target.closest(".nav__item");
+
+    if (target) toggleActive();
+  });
+}
